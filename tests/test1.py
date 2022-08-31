d4d03b29baaab8cd77e070200a3726fcd82f5c0b
michaelgetachew <mikygetyos@gmail.com>
2022-09-01 00:59:22 +0300

    Feat(add): added the methods for checking object creattion and duplicates

--- tests/test1.py
+++ tests/test1.py
@@ -5,3 +5,16 @@ import numpy as np
 import pandas as pd
 import pandas.api.types as ptypes
 
+from ..scripts.data_info_and_preprocessing import dataInfo
+from ..scripts.data_info_and_preprocessing import data_preprocessing
+
+class TestCases(unittest.TestCase):
+
+    def test_class_creation(self):
+        data_Info = dataInfo(self.df)
+        self.assertEqual(self.df.info(), data_Info.df.info())
+
+    def test_remove_duplicates(self):
+        data_preprocess = data_preprocessing(self.df)
+        data_preprocess.remove_duplicates()
+        self.assertEqual(data_preprocess.df.shape[0], self.df.shape[0])
